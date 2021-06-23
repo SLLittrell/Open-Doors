d24937308f2b8bda82429685b3255c0f5ce38029
sllittrell <stacey.littrell@gmail.com>
2021-06-23 18:04:21 -0500

    commit

--- server/open_doors/settings.py
+++ server/open_doors/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+# import environ
+
+# env = environ.Env()
+# environ.Env.read_env()
 
 import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-SECRET_KEY= os.getenv('SECRET_KEY')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
