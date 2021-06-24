56362c81b4501bc93522ae88bdc0d874da381167
sllittrell <stacey.littrell@gmail.com>
2021-06-24 11:15:39 -0500

    post image field length change

--- open_doors_api/models/post.py
+++ open_doors_api/models/post.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     publication_date = models.DateTimeField(auto_now_add=True)
-    image_url = models.CharField(max_length=500)
+    image_url = models.TextField()
     social_story = models.ForeignKey("SocialStory", on_delete=models.CASCADE, null=True, blank=True)
     visual_schedule = models.ForeignKey("VisualSchedule", on_delete=models.CASCADE, null=True, blank=True)
     content = models.TextField()
